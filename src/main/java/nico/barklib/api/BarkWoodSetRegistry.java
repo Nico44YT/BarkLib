168bd7b134fe7ef0f9596f537899910cee1d62c8
Nico44YT <Nico_44-YouTube@gmx.de>
2025-12-18 21:02:37 +0100

    Some getters

--- src/main/java/nico/barklib/api/BarkWoodSetRegistry.java
+++ src/main/java/nico/barklib/api/BarkWoodSetRegistry.java
@@ -6,12 +6,30 @@ import net.minecraft.util.Identifier;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class BarkWoodSetRegistry {
     protected static final Map<Identifier, BarkWoodSet> map = new HashMap<>();
 
     public static void registerWoodSet(BarkWoodSet woodSet) {
         map.put(woodSet.id(), woodSet);
+    }
+
+    public static Optional<BarkWoodSet> maybeGetWoodSet(Identifier id) {
+        return Optional.ofNullable(map.get(id));
+    }
+
+    public static BarkWoodSet getWoodSet(Identifier id) {
+        return map.get(id);
+    }
+
+    public static Stream<BarkWoodSet> stream() {
+        return map.values().stream();
+    }
+
+    public static Stream<Map.Entry<Identifier, BarkWoodSet>> entryStream() {
+        return map.entrySet().stream();
     }
 
     private static boolean vanillaInit = false;
